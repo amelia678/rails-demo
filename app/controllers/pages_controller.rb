c0d8db60356e5d081ab72244d23515d88957a33b
Amelia Schulz <amelia.schulz@gmail.com>
2019-02-15 10:39:57 -0500

    Add Search Page and route.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
 
 def home
     end
-    
+
+def search
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'welcome' => 'pages#home'
+  get 'search' => 'pages#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
